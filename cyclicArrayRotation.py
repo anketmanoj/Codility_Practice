d2d9a061fb70b628359dfc929b01251aa81c199b
anketmanoj <68798781+anketmanoj@users.noreply.github.com>
2022-09-15 14:12:31 +0900

    cyclicRotation challenge done

---
+++ cyclicArrayRotation.py
@@ -0,0 +1,12 @@
+
+def solution(A, K):
+    old = A
+    new = [0]*len(A)
+    for i in range(K):
+        new[0] = old[-1]
+        new[1:]=old[:-1]
+        old = new.copy()
+    print(old)
+    return new
+
+solution([1,2,3,4,5], 2)
